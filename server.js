67d08c7d421a9101b518fa052f038ac2d85a7a53
Kismet <47130796+charlotterice@users.noreply.github.com]>
2019-06-25 20:24:52 -0400

    added beginning of scrape route

--- server.js
+++ server.js
@@ -14,4 +14,15 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static("public"));
 
-//mongoose.connect("mongodb://")
+//mongoose.connect("mongodb://localhost/unit18Populater",{useNewUrlParser: true});
+
+app.get("/scrape", function(req,res){
+    axios.get("https://www.bbc.com/").then(function(response){
+        var $ = cheerio.load(response.data);
+        $("article h3").each(function(i, element){
+            var result ={};
+            result.title = $(this).children("a").text();
+            result.link = $(this).children("a").attr("href");
+        })
+    })
+})
