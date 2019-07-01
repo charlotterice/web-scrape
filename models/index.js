644685f22e9ba80f7f4950f37b5ae96ae0ca01cd
Kismet <47130796+charlotterice@users.noreply.github.com]>
2019-07-01 18:38:39 -0400

    working on article

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 // Exporting an object containing all of our models
 
 module.exports = {
-  Article: require("web-scrape/model/Article"),
+  Article: require("web-scrape/models/Article"),
   Note: require("./Note")
 };
