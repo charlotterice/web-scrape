a400ab04f32be4bf65400117f2d52556c6e31a2b
Kismet <47130796+charlotterice@users.noreply.github.com]>
2019-07-01 18:15:06 -0400

    trying to fix Article Route

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 // Exporting an object containing all of our models
 
 module.exports = {
-  Article: require(__dirname + "/Article"),
+  Article: require("../models/Article"),
   Note: require("./Note")
 };
